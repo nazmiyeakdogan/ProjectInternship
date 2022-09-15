891942be67fa92bcf6e0b447cc43ff18a2d5a6fc
user <pln.ozyolc@hotmail.com>
2022-09-15 16:00:41 +0300

    TEC_25, TEC_26  ve Runner Classlar  Tamamlandı

---
+++ src/test/java/StepDefinitions/TEC_25_Steps.java
@@ -0,0 +1,24 @@
+package StepDefinitions;
+
+import Pages.DialogContent;
+import Pages.LeftNav;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class TEC_25_Steps {
+    DialogContent dc=new DialogContent();
+  // @And("User delete item from Dialog Content")
+  // public void userDeleteItemFromDialogContent(DataTable elements) {
+
+  //     List<String> listElement = elements.asList(String.class);
+
+  //     for (int i = 0; i < listElement.size(); i++)
+  //     {
+  //         dc.SearchAndDelete(listElement.get(i));
+  //     }
+  // }
+  }
+//
